03181d2d4e0054fd68f669e4bed83071d8f7f838
Lee <swiniee@naver.com>
2020-11-02 15:53:03 +0900

    @Autowired(required = false)

--- src/main/java/com/cos/lectureex/BookSerivce.java
+++ src/main/java/com/cos/lectureex/BookSerivce.java
@@ -6,12 +6,18 @@ public class BookSerivce {
 
 	// BookService가 BookRepository를 사용한다.
 	@Autowired // 의존 관계 설정
+	//BookRepository bookRepository;
+	
+	// 1. 생성자로
+//	BookRepository bookRepository;
+//	
+//	public BookSerivce(BookRepository bookRepository) {
+//		this.bookRepository = bookRepository;
+//	}
+	
+	
+	// 2. setter로
 	BookRepository bookRepository;
-	
-	
-	public BookSerivce(BookRepository bookRepository) {
-		this.bookRepository = bookRepository;
-	}
 	
 	public void setBookRepository(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
