121cb1543b897fd5a8bc26b762206365ca91a62c
Lee <swiniee@naver.com>
2020-11-03 21:52:14 +0900

    messageSource

--- src/main/java/com/cos/lectureex/LectureexApplication.java
+++ src/main/java/com/cos/lectureex/LectureexApplication.java
@@ -2,20 +2,33 @@ package com.cos.lectureex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
+//@PropertySource("classpath:/app.properties")
 public class LectureexApplication {
 
 	public static void main(String[] args) {
-		
 
 		SpringApplication.run(LectureexApplication.class, args);
-		//ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-		//context.
-		
-		//var app = new SpringApplication(LectureexApplication.class);
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("application.xml");
+		// context.
+	}
+	// var app = new SpringApplication(LectureexApplication.class);
+
 	
+	// 릴로딩 기능이 있는 메세지 소스 사용하기
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/messages");
+		messageSource.setDefaultEncoding("UTF-8"); // 한글 깨짐 설정
+		messageSource.setCacheSeconds(3); // 리소스를 3초 캐싱하고 다시 읽도록..
+		return messageSource;
 	}
 
 }
---
+++ src/main/java/com/cos/lectureex/messagesource/AppRunner2.java
@@ -0,0 +1,32 @@
+package com.cos.lectureex.messagesource;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner2 implements ApplicationRunner {
+
+	@Autowired
+	MessageSource messageSource;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		//System.out.println(messageSource.getClass());
+		// 바로 쓸 수 있음
+		while(true) {
+			System.out.println(messageSource.getMessage("greeting", new String[] {"lee"}, Locale.KOREA));
+			System.out.println(messageSource.getMessage("greeting", new String[] {"lee"}, Locale.getDefault()));
+			Thread.sleep(1000l); // 1초마다 찍는다
+		}
+		
+		
+	}
+	
+	
+}
